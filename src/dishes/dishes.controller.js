62d97347a26cb922a621034636bea08c4b42f946
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-15 13:18:51 -0400

    update controllers and routers

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,163 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function create(req, res, next) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+
+  if (!name || name === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a name",
+    });
+  }
+
+  if (!description || description === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a description",
+    });
+  }
+
+  if (!price || typeof price !== "number" || price <= 0) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  }
+
+  if (!image_url || image_url === "") {
+    return next({
+      status: 400,
+      message: "Dish must include an image_url",
+    });
+  }
+
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function read(req, res) {
+  const dishId = req.params.dishId;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.json({ data: foundDish });
+  } else {
+    res.status(404).json({ error: `Dish not found with id: ${dishId}` });
+  }
+}
+
+function update(req, res, next) {
+  const dishId = req.params.dishId;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (!foundDish) {
+    return next({
+      status: 404,
+      message: `Dish with id ${dishId} not found`,
+    });
+  }
+
+  const { data: { id, name, description, price, image_url } = {} } = req.body;
+
+  if (id && id !== dishId) {
+    return next({
+      status: 400,
+      message: `Dish id ${id} does not match :dishId ${dishId}`,
+    });
+  }
+
+  if (!name || name === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a name",
+    });
+  }
+
+  if (!description || description === "") {
+    return next({
+      status: 400,
+      message: "Dish must include a description",
+    });
+  }
+
+  if (!image_url || image_url === "") {
+    return next({
+      status: 400,
+      message: "Dish must include an image_url",
+    });
+  }
+
+  if (typeof price !== "number") {
+    return next({
+      status: 400,
+      message: "Dish price must be a number",
+    });
+  }
+
+  if (price <= 0) {
+    return next({
+      status: 400,
+      message: "Dish price must be greater than 0",
+    });
+  }
+
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+
+  res.json({ data: foundDish });
+}
+
+
+function destroy(req, res, next) {
+  const dishId = req.params.dishId;
+  const foundDishIndex = dishes.findIndex((dish) => dish.id === dishId);
+
+  if (foundDishIndex === -1) {
+    return next({
+      status: 405,
+      message: `Dish with id ${dishId} does not exist`,
+    });
+  }
+
+  const deletedDish = dishes.splice(foundDishIndex, 1)[0];
+
+  res.status(405).json({
+    status: 405,
+    error: `Dish with id ${dishId} has been deleted`,
+  });
+}
+
+
+
+
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+  destroy,
+};
+
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+  destroy,
+};
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,189 @@
 const path = require("path");
-
-// Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
-
-// Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res, next) {
+  res.json({ data: orders });
+}
+
+function create(req, res, next) {
+  const { data: { deliverTo, mobileNumber, dishes } = {} } = req.body;
+
+  const validStatuses = ["pending", "preparing", "out-for-delivery", "delivered"];
+  const status = "pending"; // Set default status to "pending"
+
+  if (!deliverTo || deliverTo === "") {
+    return next({
+      status: 400,
+      message: "Order must include a deliverTo",
+    });
+  }
+
+  if (!mobileNumber || mobileNumber === "") {
+    return next({
+      status: 400,
+      message: "Order must include a mobileNumber",
+    });
+  }
+
+  if (!dishes || !Array.isArray(dishes) || dishes.length === 0) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  for (let index = 0; index < dishes.length; index++) {
+    const dish = dishes[index];
+    if (!("quantity" in dish)) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+
+    if (typeof dish.quantity !== "number" || dish.quantity <= 0) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    dishes,
+    status,
+  };
+
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+
+function read(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (foundOrder) {
+    res.json({ data: foundOrder });
+  } else {
+    next({
+      status: 404,
+      message: `Order with id ${orderId} not found.`,
+    });
+  }
+}
+
+function update(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  const validStatuses = ["pending", "preparing", "out-for-delivery", "delivered"];
+
+  if (!foundOrder) {
+    return next({
+      status: 404,
+      message: `Order with id ${orderId} not found.`,
+    });
+  }
+
+  const { data: { id, status, ...order } = {} } = req.body;
+
+  if (id && id !== orderId) {
+    return next({
+      status: 400,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}.`,
+    });
+  }
+
+  if (status === "delivered" && foundOrder.status === "delivered") {
+    return next({
+      status: 400,
+      message: "A delivered order cannot be changed.",
+    });
+  }
+  
+  if (!status || status === undefined || !validStatuses.includes(status)) {
+    return next ({
+      status: 400,
+      message: "status"
+    })
+  }
+
+  if (!order.deliverTo || order.deliverTo === "") {
+    return next({
+      status: 400,
+      message: "Order must include a deliverTo",
+    });
+  }
+
+  if (!order.mobileNumber || order.mobileNumber === "") {
+    return next({
+      status: 400,
+      message: "Order must include a mobileNumber",
+    });
+  }
+
+  if (!order.dishes || !Array.isArray(order.dishes) || order.dishes.length === 0) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  for (let index = 0; index < order.dishes.length; index++) {
+    const dish = order.dishes[index];
+    if (!("quantity" in dish)) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+
+    if (typeof dish.quantity !== "number" || dish.quantity <= 0) {
+      return next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
+
+  foundOrder.deliverTo = order.deliverTo;
+  foundOrder.mobileNumber = order.mobileNumber;
+  foundOrder.status = status; // Update the status
+  foundOrder.dishes = order.dishes;
+
+  res.json({ data: foundOrder });
+}
+
+
+function destroy(req, res, next) {
+  const { orderId } = req.params;
+  const foundIndex = orders.findIndex((order) => order.id === orderId);
+
+  if (foundIndex === -1) {
+    return next({
+      status: 404,
+      message: `Order with id ${orderId} not found.`,
+    });
+  }
+
+  if (orders[foundIndex].status !== "pending") {
+    return next({
+      status: 400,
+      message: "An order cannot be deleted unless it is pending.",
+    });
+  }
+
+  orders.splice(foundIndex, 1);
+  res.sendStatus(204);
+}
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+  destroy,
+};
